6433692f9576e49af9ac9bb159391e1576ee7c80
mariann-regio <mariannaregio@gmail.com>
2023-08-08 17:35:54 -0300

    correction

--- src/index.ts
+++ src/index.ts
@@ -81,15 +81,14 @@ function showRepositories(user: GitHubUser) {
             Nome do repositório: ${user.repos[i].name}
             Descrição: ${user.repos[i].description}
             Estrelas: ${user.repos[i].stargazers_count}
-            É um fork: ${user.repos[i].fork}
+            É um fork: ${user.repos[i].fork ? "Sim" : "Não"}
             `);
   }
 }
 
 //mostrar todos os usuários salvos
 function showAllUsers() {
-  const message = "Usuários:\n";
-  console.log(message);
+  console.log("Usuários:\n");
 
   users.forEach((user) => {
     console.log(
@@ -106,10 +105,10 @@ function getAllRepositories() {
 
 //mostrar os tres usuarios com mais repositorios
 async function getUsersMostRepos() {
-  // Ordena o array de usuários com base no número de repositórios em ordem decrescente
-  const sortedUsers = users.sort((a, b) => b.public_repos - a.public_repos);
+  // copia o array de usuários e ordena o novo com base no número de repositórios em ordem decrescente
+  const sortedUsers = users.slice().sort((a, b) => b.public_repos - a.public_repos);
   // Retorna os tres primeiros usuários do array ordenado
-  const top5 = sortedUsers.slice(0, 3).forEach((user,index) =>
+  const topFive = sortedUsers.slice(0, 3).forEach((user,index) =>
     console.log(`${index+1}°: ${user.name}
         Repositórios: ${user.public_repos}`)
   );
